e5564a746d9413074e5191eb287a1f917762da90
최갑주 <62132388+Choigapju@users.noreply.github.com>
2024-03-30 21:22:35 +0900

    Update code.py

--- 프로그래머스  /숫자 문자열과 영단어 /code.py
+++ 프로그래머스  /숫자 문자열과 영단어 /code.py
@@ -1,1 +1,8 @@
+def solution(s):
 
+    number = ['zero','one','two','three','four','five','six','seven','eight','nine']
+    
+    for i in range(len(number)):
+        s = s.replace(number[i], str(i))
+            
+    return int(s)
