5b0f0b43182577fa487f11e28566f459e320874f
최갑주 <62132388+Choigapju@users.noreply.github.com>
2024-04-09 10:32:14 +0900

    덧칠하기code.py

---
+++ 프로그래머스  /99클럽 /덧칠하기 /code.py
@@ -0,0 +1,12 @@
+def solution(n, m, section):
+    answer = 0
+    tmp = 0
+    for i in range(len(section)):
+        if section[i] <= tmp:
+            continue
+            
+        else:
+            answer += 1
+            tmp = section[i] + m -1
+    
+    return answer
